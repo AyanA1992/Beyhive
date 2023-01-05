26f7c8dbe79283c8db7ffc98c946eeea1bd319cd
ayanahmed92 <ayanaaa1992@gmail.com>
2023-01-05 01:51:36 -0600

    Added images

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Beyhive
+# Beyhive Project 2
+
+A music blog web application for Beyonce's music. 
+
+## Function
+Click the album title to view review + tracklist. Click the 'edit' buttton to edit a review. Click the 'share your thoughts' button to submit a review. Click the delete button to remove an album
+
+## Technologies Used
+* HTML
+* CSS
+* Express
+* MongoDB
+
+## Getting Started
+
+
+## Links/Sources
+* https://music.apple.com/us/artist/beyonc%C3%A9/1419227
+
+* Google images
+
+## Future Enhancements
+-updating css
+
+
+## Screenshots
+<img src="https://ibb.co/LZmKRJ1"># Beyhive
--- models/data.js
+++ models/data.js
@@ -14,7 +14,7 @@ const data = [
         name:"B'Day",
         year: 2006,
         img:"https://i.ibb.co/JpW4Lj1/beyonce-bday.jpg" ,
-        Review:"",
+        Review:"TOP 3 FAV album. There isnt a song in this album I would skip. She truly put her foot in this.",
         songlist:['Deja Vu', 'Get Me Bodied', 'Suga Mama', 'Upgrade U', 'Ring The Alarm', 'Kitty Kat', 'Freakum Dress', 'Green Light', 'Irreplaceable', 'Resentment', 'Check On It', 'Encore for the Fans', 'Listen', 'Get Me Bodied Extended Mix'  ]
         
 
@@ -24,7 +24,7 @@ const data = [
         name:"I Am Sasha Fierce",
         year:2008,
         img:"https://i.ibb.co/25Tp0RJ/I-Am-Sasha-Fierce.png" ,
-        Review:'',
+        Review:'5 STARS',
         songlist: ['If I Were A Boy', 'Halo', 'Disappear', 'Broken-Hearted Girl', 'Ava Maria', 'Smash Into You', 'Satellites', 'Thats Why Youre Beautiful', 'Save the Hero', 'Single Ladies', 'Radio', 'Diva', 'Sweet Dreams', 'Video Phone', 'Ego', 'Scared of Lonely']
 
     },
@@ -33,7 +33,7 @@ const data = [
         name:"4",
         year:2011,
         img:"https://i.ibb.co/G707rVV/Beyonce-4.png",
-        Review:'',
+        Review:'Love on top, dance for you, and Run the world were my favs from this album. 5/5',
         songlist: ['1+1', 'I Care', 'I Miss You', 'Best Thing I Never Had', 'Party', 'Rather Die Young', 'Start Over', 'Love on Top', 'Countdown', 'End of Time', 'I was Here', 'Run the World', 'Lay Up Under Me', 'Schoolin Life', 'Dance for You', 'Run the World(Girls)',   ]
         
 
@@ -59,7 +59,7 @@ const data = [
         name:"The Lion King: The Gift",
         year:2019,
         img:"https://i.ibb.co/B4jzMHP/thegift.jpg" ,
-        Review:'',
+        Review:'Love love love this album, - it was originally a soundtrack for the new Lion King movie and she had a ton of features from Afro Beats artists like Burna Boy,Yemi Alade, Wizkid- TOP TIER CONTENT!',
         songlist:['Bigger', 'Find Your Way Back', 'Dont Jealous Me', 'JA ARA E', 'Nile', 'Mood 4Eva', 'Water', 'Brown Skin Girl', 'Keys to The Kingdom', 'Already', 'Otherside', 'My Power', 'Scar', 'Spirit']
     
 
@@ -68,7 +68,7 @@ const data = [
         name:"Renaissance",
         year:2022,
         img:"https://i.ibb.co/K6wsRgJ/beyonce-renaissance-sq-44bf2e820ab82236e9b3615b90a551eb03bed143-s1100-c50.jpg" ,
-        Review: '',
+        Review: 'SENSATIONAL. Every song is an absolute bop. The transitions are amazing!!',
         songlist: ['Im That Girl', 'Cozy', 'Alien Superstar', 'Cuff It', 'Energy', 'Break My Soul', 'Church Girl', 'Plastic Off The Sofa', 'Virgo Groove', 'Move', 'Heated', 'Thique', 'All Up In Your Mind', 'America Has A Problem', 'Pure/Honey', 'Summer Renaissance']
     },
  ]
